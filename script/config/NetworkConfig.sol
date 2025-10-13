007d129f17f41719268511963e434ca7afa45593
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-10-13 10:54:59 +0200

    feat: added VestingScheduler config

--- script/config/NetworkConfig.sol
+++ script/config/NetworkConfig.sol
@@ -9,7 +9,7 @@ library NetworkConfig {
         address treasury;
         address distributor;
         // External Contracts Settings
-        // address vestingScheduler;
+        address vestingScheduler;
         address superTokenFactory;
         address positionManager;
         address poolManager;
@@ -44,7 +44,7 @@ library NetworkConfig {
             admin: address(0),
             treasury: address(0),
             distributor: address(0),
-            // vestingScheduler: 0x7b77A34b8B76B66E97a5Ae01aD052205d5cbe257,
+            vestingScheduler: 0x6Bf35A170056eDf9aEba159dce4a640cfCef9312,
             superTokenFactory: 0xe20B9a38E0c96F61d1bA6b42a61512D56Fea1Eb3,
             positionManager: 0x7C5f5A4bBd8fD63184577525326123B519429bDc,
             poolManager: 0x498581fF718922c3f8e6A244956aF099B2652b2b,
@@ -53,7 +53,7 @@ library NetworkConfig {
             spiritTokenSymbol: "SPIRIT",
             spiritTokenSupply: 1_000_000_000 ether,
             spiritTokenLiquiditySupply: 250_000_000 ether,
-            spiritInitialTick: 230_600,
+            spiritInitialTick: 184_216,
             spiritPoolFee: 10_000,
             spiritTickSpacing: 200
         });
@@ -71,16 +71,16 @@ library NetworkConfig {
             admin: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
             treasury: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
             distributor: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
-            // vestingScheduler: 0xF8c4eB09b51DD85cE315f76132F17f8FA5073e54,
+            vestingScheduler: 0x2D0B7a30bFdED086571D6525762a809ee1049c98,
             superTokenFactory: 0x7447E94Dfe3d804a9f46Bf12838d467c912C8F6C,
             positionManager: 0x4B2C77d209D3405F41a037Ec6c77F7F5b8e2ca80,
             poolManager: 0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408,
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
-            spiritTokenName: "Secret Token V2",
-            spiritTokenSymbol: "SECRETv2",
+            spiritTokenName: "Secret Token V3",
+            spiritTokenSymbol: "SECRETv3",
             spiritTokenSupply: 1_000_000_000 ether,
             spiritTokenLiquiditySupply: 250_000_000 ether,
-            spiritInitialTick: 230_600,
+            spiritInitialTick: 184_216,
             spiritPoolFee: 10_000,
             spiritTickSpacing: 200
         });
@@ -94,15 +94,16 @@ library NetworkConfig {
             admin: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
             treasury: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
             distributor: 0x5D6D8518A1d564c85ea5c41d1dc0deca70F2301C,
+            vestingScheduler: 0x638a8ABF60118e018c80a0eC878057E8C53E0fd1,
             positionManager: 0x429ba70129df741B2Ca2a85BC3A2a3328e5c09b4,
             poolManager: 0xE03A1074c86CFeDd5C142C4F04F1a1536e203543,
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
             superTokenFactory: 0x254C2e152E8602839D288A7bccdf3d0974597193,
-            spiritTokenName: "Secret Token V2",
-            spiritTokenSymbol: "SECRETv2",
+            spiritTokenName: "Secret Token V3",
+            spiritTokenSymbol: "SECRETv3",
             spiritTokenSupply: 1_000_000_000 ether,
             spiritTokenLiquiditySupply: 250_000_000 ether,
-            spiritInitialTick: 230_600,
+            spiritInitialTick: 184_216,
             spiritPoolFee: 10_000,
             spiritTickSpacing: 200
         });
